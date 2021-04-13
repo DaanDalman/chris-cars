7395bc87624376a3fc91906becb0a980c729667f
Daan Dalman <daandalman@gmail.com>
2021-04-13 14:13:12 +0200

    created edit items page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -28,7 +28,7 @@ class ItemsController < ApplicationController
       def update
         @item = Item.find(params[:id])
         @item.update(item_params)
-        redirect_to car_path(@item)
+        redirect_to item_path(@item)
       end
     
       def destroy
